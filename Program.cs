e108269acca0edcb60531eed293851b557dc4c06
Анастасия <anastasia@MacBook-Air-Anastasia-2.local>
2026-04-07 12:31:54 +0400

    apply migrations on startup

--- Program.cs
+++ Program.cs
@@ -30,6 +30,8 @@ app.MapControllerRoute(
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    db.Database.Migrate();
     SeedData.Initialize(db);
 }
 
